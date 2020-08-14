a9f818117da31b3c9bee944ca9a95adc21a9d568
BensonChen <472756921@qq.com>
2020-08-14 14:53:48 +0800

    x

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+	var a = '123';
 	return (
 		<div>
 			Home
