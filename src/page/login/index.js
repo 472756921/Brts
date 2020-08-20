2d1a60004cd64a88d53171979dc3afd978648c53
BensonChen <472756921@qq.com>
2020-08-20 17:46:56 +0800

    add husky

--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Styles from "./index.css";
+import React from 'react';
+import Styles from './index.css';
 
 const login = () => {
-  return <div className={Styles.content_login}>Login</div>;
+	return <div className={Styles.content_login}>Login</div>;
 };
 
 export default login;
